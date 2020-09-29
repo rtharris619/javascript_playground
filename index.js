7d6b8d6bfc2b908d2f780c209de6cf7427335e76
rtharris619 <harris.ryantrevor@gmail.com>
2020-09-29 08:05:40 +0200

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+const person = {
+  name: {
+    first: 'Bob',
+    last: 'Smith',
+  },
+  age: 32,
+  gender: 'male',
+  interests: ['music', 'skiing'],
+  bio: function () {
+    console.log(this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old.');
+  },
+  greeting: function () {
+    console.log('Hi! I\'m ' + this.name.first + '.');
+  }
+};
+
+console.log(person.name.first);
+person.bio();
+person.greeting();
